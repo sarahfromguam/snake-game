7aa13c04c385b2cf61c46e5f499582b87ee39f75
Sarah Wilkinson <70497480+sarahfromguam@users.noreply.github.com>
2021-08-11 12:14:35 +1000

    Add replay feature

--- main.py
+++ main.py
@@ -4,44 +4,58 @@ from snake import Snake
 from food import Food
 from score import Scoreboard
 
+# Set up screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake()
-food = Food()
-score = Scoreboard()
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+def play():
+    game_on = True
 
-game_on = True
-while game_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    screen.reset()
+    # Initialize objects
+    snake = Snake()
+    food = Food()
+    score = Scoreboard()
 
-    # Detect collision with food
-    if snake.head.distance(food) < 17:
-        food.refresh()
-        score.add_score()
-        snake.extend()
+    # Add key functionalities
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-    # Detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -290:
-        score.game_over()
-        game_on = False
+    while game_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_on = False
+        # Detect collision with food
+        if snake.head.distance(food) < 17:
+            food.refresh()
+            score.add_score()
+            snake.extend()
+
+        # Detect collision with wall
+        if snake.head.xcor() > 290 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -290:
             score.game_over()
+            game_on = False
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_on = False
+                score.game_over()
+
+
+play()
+
+screen.onkey(play, "space")
+
+screen.exitonclick()
 
 
 screen.exitonclick()
