70e2a86cdf6d75e3da3117f7c9cee054dd72a08a
Sarah Wilkinson <70497480+sarahfromguam@users.noreply.github.com>
2021-08-11 12:15:35 +1000

    update score.py

--- score.py
+++ score.py
@@ -1,9 +1,13 @@
 from turtle import Turtle
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.score = 0
+        with open(file="data.txt") as data:
+            self.high_score = int(data.read())
+        self.score = -1
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -11,20 +15,27 @@ class Scoreboard(Turtle):
         self.shape(None)
         self.speed(0)
         self.add_score()
-        self.high_score = 0
-        with open(file="data.txt") as data:
-            self.high_score = int(data.read())
-
-    def game_over(self):
-        self.goto(0,0)
-        self.color("red")
-        self.write("GAME OVER", align="center", font=("Courier", 30, "normal"))
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align="center", font = ("Courier", 20, "normal"))
+        self.write(f"Score: {self.score} | High Score: {self.high_score}", move=False, align="center", font = ("Courier", 20, "normal"))
+
+    def game_over(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            # Rewrite text file with high score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+        self.goto(0, 0)
+        self.color("red")
+        self.write("GAME OVER", align="center", font=("Courier", 30, "normal"))
+        self.goto(0, -20)
+        self.write("Click the space bar to go again.", align="center", font=("Courier", 15, "normal"))
+        self.goto(0, -40)
+        self.write("Click the screen to exit.", align="center", font=("Courier", 15, "normal"))
 
     def add_score(self):
         self.score += 1
-        if self.score > self.high_score:
-            self.high_score = self.score
+        self.update_scoreboard()
